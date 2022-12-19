9fb0a617ba2cee354ddd414fe019d17953cb0522
mana0902 <todamana0902@gmail.com>
2022-12-19 23:11:25 +0900

    Posts Route,view

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('posts', PostsController::class)
+->middleware('auth');
 
 Route::get('/', function () {
     return view('welcome');
